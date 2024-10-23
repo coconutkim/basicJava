16dd5df63a539172bb46c388538887a224cc9dcb
coconutkim <achaeyeon18@gmail.com>
2024-10-23 20:26:01 +0900

    1023

---
+++ src/oop/excep/Exception3.java
@@ -0,0 +1,22 @@
+package oop.excep;
+
+public class Exception3 {
+
+    public static void main(String[] args){
+        try{
+            Class.forName("java.lang.String");
+            System.out.println("there's class of java.lang.String");
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+
+        System.out.println();
+
+        try{
+            Class.forName("java.lang.String2");
+            System.out.println("there's class of java.lang.String2");;
+        }catch (ClassNotFoundException e){
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/oop/excep/MyException.java
@@ -0,0 +1,9 @@
+package oop.excep;
+
+import java.util.InputMismatchException;
+
+public class MyException extends Exception {
+    public MyException(String ms){
+        super(ms);
+    }
+}
